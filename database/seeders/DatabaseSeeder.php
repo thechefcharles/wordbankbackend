cd6a850173bd426f24caf8104779717871731c6d
thechefcharles <charlieforeman77@gmail.com>
2025-02-07 13:32:48 -0600

    Update autoloader

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+// File: database/seeders/DatabaseSeeder.php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // You can call other seeders here, e.g.,
+        // $this->call(PhraseSeeder::class);
+    }
+}
